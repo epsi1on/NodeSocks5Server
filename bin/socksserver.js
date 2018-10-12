6195658239ae7bcbbbf6ed4cf252c96610a13f12
epsi1on <ehsan.ma@gmx.com>
2018-10-12 13:55:42 +0330

    update

--- bin/socksserver.js
+++ bin/socksserver.js
@@ -1,13 +1,11 @@
 
-var addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/;
+var addrRegex = /^([a-zA-Z\-\.0-9]+)?:(\d+)$/;
 
-//var argv = require('minimist')(process.argv.slice(2));
+var argv = require('minimist')(process.argv.slice(2));
 
 //var openshift_host = null;
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+//
 
 
 //if( process.env.OPENSHIFT_NODEJS_IP != null && process.env.OPENSHIFT_NODEJS_PORT != null)
@@ -15,12 +13,24 @@ var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 //console.log('Openshift ip port: ' + (process.env.OPENSHIFT_NODEJS_IP+':'+process.env.OPENSHIFT_NODEJS_PORT));
 
-//var listenOn = openshift_host || argv.l || process.env.l;
+var listenOn = argv.l || process.env.l;
+
+if(!addrRegex.test(listenOn))
+{
+
+    console.log('invalid address: '+listenOn);
+    process.exit(1);
+}
+
+var port = listenOn.match(addrRegex)[2];// process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var ip = listenOn.match(addrRegex)[1];//process.env.IP || process.env.OPENSHIFT_NODEJS_IP || OPENSHIFT_DIY_IP || '0.0.0.0';
 
 //if(listenOn != null)
 //    listenOn = listenOn.match(addrRegex);
 
-var listenOn = ip.toString() + ':' + port.toString();
+
+//var listenOn = ip.toString() + ':' + port.toString();
 
 
 if(listenOn == null )
